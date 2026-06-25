e3bc2a06990f9469692e4744837e564900421363
Illia Medvedev <bearillia@gmail.com>
2026-06-25 19:44:25 +0100

    add global validation pipe, request body validation for /candidates endpoint

--- src/create-candidate.dto.ts
+++ src/create-candidate.dto.ts
@@ -1,7 +1,16 @@
+import { Type } from 'class-transformer';
+import { IsEmail, IsInt, IsNotEmpty, IsString, Min } from 'class-validator';
+
 export class CreateCandidateDto {
+  @IsString()
+  @IsNotEmpty()
   name: string;
 
+  @IsEmail()
   email: string;
 
+  @Type(() => Number)
+  @IsInt()
+  @Min(0)
   experienceYears: number;
 }
